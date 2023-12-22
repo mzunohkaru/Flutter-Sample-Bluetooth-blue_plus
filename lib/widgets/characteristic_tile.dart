7a913adf606af0e175f365cd55cb2a3db4eabc18
mzunohkaru <winkaisyu@gmail.com>
2023-12-22 19:42:55 +0900

    third commid

--- lib/widgets/characteristic_tile.dart
+++ lib/widgets/characteristic_tile.dart
@@ -6,12 +6,13 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:flutter_blue_plus_sample/utils.dart/snackbar.dart';
 import 'package:flutter_blue_plus_sample/widgets/descriptor_tile.dart';
 
-
 class CharacteristicTile extends StatefulWidget {
   final BluetoothCharacteristic characteristic;
   final List<DescriptorTile> descriptorTiles;
 
-  const CharacteristicTile({Key? key, required this.characteristic, required this.descriptorTiles}) : super(key: key);
+  const CharacteristicTile(
+      {Key? key, required this.characteristic, required this.descriptorTiles})
+      : super(key: key);
 
   @override
   State<CharacteristicTile> createState() => _CharacteristicTileState();
@@ -25,7 +26,8 @@ class _CharacteristicTileState extends State<CharacteristicTile> {
   @override
   void initState() {
     super.initState();
-    _lastValueSubscription = widget.characteristic.lastValueStream.listen((value) {
+    _lastValueSubscription =
+        widget.characteristic.lastValueStream.listen((value) {
       _value = value;
       setState(() {});
     });
@@ -39,9 +41,16 @@ class _CharacteristicTileState extends State<CharacteristicTile> {
 
   BluetoothCharacteristic get c => widget.characteristic;
 
+  // 書き込むデータは4バイトのランダムなデータ
   List<int> _getRandomBytes() {
     final math = Random();
-    return [math.nextInt(255), math.nextInt(255), math.nextInt(255), math.nextInt(255)];
+    // 0から255までのランダムな整数を4つ生成し、リストとして返す
+    return [
+      math.nextInt(255),
+      math.nextInt(255),
+      math.nextInt(255),
+      math.nextInt(255)
+    ];
   }
 
   Future onReadPressed() async {
@@ -55,9 +64,13 @@ class _CharacteristicTileState extends State<CharacteristicTile> {
 
   Future onWritePressed() async {
     try {
-      await c.write(_getRandomBytes(), withoutResponse: c.properties.writeWithoutResponse);
+      // Bluetoothの特性（Characteristic）にランダムなバイトデータを書き込む
+      await c.write(_getRandomBytes(),
+          // 書き込み操作が応答なし（Response）で行われるべきかどうかを示します
+          withoutResponse: c.properties.writeWithoutResponse);
       Snackbar.show(ABC.c, "Write: Success", success: true);
       if (c.properties.read) {
+        // 特性の現在の値を読み取り
         await c.read();
       }
     } catch (e) {
@@ -67,24 +80,33 @@ class _CharacteristicTileState extends State<CharacteristicTile> {
 
   Future onSubscribePressed() async {
     try {
+      // 現在の通知の状態に基づいて、次の操作が Subscribe か Unubscribe かを決定します
       String op = c.isNotifying == false ? "Subscribe" : "Unubscribe";
+      // 特性の通知の状態を切り替えます
+      // c.isNotifying == falseの場合、通知を開始（Subscribe）します。それ以外の場合は通知を停止（Unubscribe）します
       await c.setNotifyValue(c.isNotifying == false);
+      // 通知の状態の切り替えが成功した場合
       Snackbar.show(ABC.c, "$op : Success", success: true);
       if (c.properties.read) {
+        // 特性の現在の値を読み取り
         await c.read();
       }
       setState(() {});
     } catch (e) {
-      Snackbar.show(ABC.c, prettyException("Subscribe Error:", e), success: false);
+      Snackbar.show(ABC.c, prettyException("Subscribe Error:", e),
+          success: false);
     }
   }
 
   Widget buildUuid(BuildContext context) {
+    // Bluetoothの特性（Characteristic）のUUID（Universally Unique Identifier）を取得し、それを大文字の16進数形式の文字列に変換しています
+    // '0x${...}'という形式で結果を文字列に組み込みます。ここで0xは16進数を表すプレフィックスです
     String uuid = '0x${widget.characteristic.uuid.str.toUpperCase()}';
     return Text(uuid, style: TextStyle(fontSize: 13));
   }
 
   Widget buildValue(BuildContext context) {
+    // リストの内容を文字列に変換
     String data = _value.toString();
     return Text(data, style: TextStyle(fontSize: 13, color: Colors.grey));
   }
